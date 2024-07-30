154d4944b5645b0b213877f924e91ccf154d3663
mayasato0401 <maya.s0401.wdr@gmail.com>
2024-07-30 23:13:39 +0900

    Add files via upload

---
+++ SOCIALTECH-jQuery/script.js
@@ -0,0 +1,15 @@
+$('.button-more').on('mouseover', function(){
+    $(this).animate({
+        opacity: 0.5,
+        marginLeft: 20,
+    },100);
+});
+
+$('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+});
+
